9d6988b4ee0c987bc0421df8942af81002a3d0f8
ChaileyB <32521607+ChaileyB@users.noreply.github.com>
2017-12-08 17:02:10 +0000

    Formatting, PHP, content

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+
+<?php
+
+if ($_POST['submit']) {
+    if (!$_POST['name']) {
+        $error="Please enter your name";
+    }
+    if (!$_POST['email']) {
+        $error="Please enter your email address";
+
+    }
+    if (!$_POST['message']) {
+        $error="Please enter a message";
+    }
+    if (!$_POST['check']) {
+        $error="Please confirm that you are not a robot";
+    }
+
+    if ($error) {
+        $result="Whoops! We found an error!";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -101,62 +125,24 @@
                             <h3>Contact charlie george</h3>
                         </div>
                         <div class="modal-body">
- <div class="messages"></div>
 
-    <div class="controls">
+                            <?php echo $result;?>
 
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_name">Name *</label>
-                    <input id="form_name" type="text" name="name" class="form-control" placeholder="Please enter your name *" required="required" data-error="Firstname is required.">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_email">Email *</label>
-                    <input id="form_email" type="email" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required.">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label for="form_phone">Phone</label>
-                    <input id="form_phone" type="tel" name="phone" class="form-control" placeholder="Please enter your phone">
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label for="form_message">Message *</label>
-                    <textarea id="form_message" name="message" class="form-control" placeholder="Message for me *" rows="4" required="required" data-error="Please,leave us a message."></textarea>
-                    <div class="help-block with-errors"></div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <input type="submit" class="btn btn-success btn-send" value="Send message">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="text-muted"><strong>*</strong> These fields are required.
-            </div>
-        </div>
-    </div>
 
-</form>
+                            <!-- Do not change the code! -->
+                            <a id="foxyform_embed_link_912660" href="http://www.foxyform.com/">foxyform</a>
+                            <script type="text/javascript">
+                                (function(d, t){
+                                    var g = d.createElement(t),
+                                        s = d.getElementsByTagName(t)[0];
+                                    g.src = "http://www.foxyform.com/js.php?id=912660&sec_hash=dd8bb540039&width=350px";
+                                    s.parentNode.insertBefore(g, s);
+                                }(document, "script"));
+                            </script>
+                            <!-- Do not change the code! -->
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-success" data-dismiss="modal">Send</button>
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                        </div>
+
                     </div>
-
                 </div>
             </div>
 
@@ -181,7 +167,7 @@
 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
             <script src="js/bootstrap.min.js"></script>
-            <script src="validator.js"></script>
-            <script src="contact.js"></script>
+            <script src="js/validator.js"></script>
+            <script src="js/contact.js"></script>
             </body>
         </html>
